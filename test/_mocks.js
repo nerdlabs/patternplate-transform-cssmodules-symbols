eebee733320204c79cd401f4eba2b1e76549085e
Mario Nebl <hello@herebecode.com>
2016-03-25 16:09:15 +0100

    test: add basic tests

---
+++ test/_mocks.js
@@ -0,0 +1,37 @@
+import unindent from 'unindent';
+
+export const application = {
+
+};
+
+export const emptyFile = {
+	buffer: new Buffer(''),
+	path: 'empty/index.jsx',
+	dependencies: {}
+};
+
+export const plainFile = {
+	buffer: new Buffer('<div />')
+};
+
+export const statelessFile = {
+	buffer: new Buffer(unindent(`
+		export default (props) => {
+			return <div />;
+		};
+	`)),
+	path: 'stateless/index.jsx'
+};
+
+export const fullFile = {
+	buffer: new Buffer(unindent(`
+	import React from 'react';
+
+	export default class FullComponent extends React.Component {
+		render() {
+			return <div />;
+		}
+	}
+	`)),
+	path: 'full/index.jsx'
+};
---
+++ test/index.js
@@ -0,0 +1,42 @@
+import 'babel-register';
+import 'babel-polyfill';
+import test from 'ava';
+
+import factory from '../source';
+import * as mocks from './_mocks';
+
+test.beforeEach(t => {
+	t.context.transform = factory(mocks.application);
+});
+
+test('it should export a function as default', t => {
+	const actual = typeof factory;
+	const expected = 'function';
+	t.same(actual, expected);
+});
+
+test('calling the function should return a function', t => {
+	const actual = typeof factory();
+	const expected = 'function';
+	t.same(actual, expected);
+});
+
+test('calling the returned function should return a promise', t => {
+	const {context: {transform}} = t;
+	const actual = transform(mocks.emptyFile).constructor.name;
+	const expected = 'Promise';
+	t.same(actual, expected);
+});
+
+test('the returned promise should resolve to an object', async t => {
+	const {context: {transform}} = t;
+	const actual = Object.prototype.toString(await transform(mocks.emptyFile));
+	const expected = '[object Object]';
+	t.same(actual, expected);
+});
+
+test('the resolved object should have buffer key', async t => {
+	const {context: {transform}} = t;
+	const file = await transform(mocks.emptyFile);
+	t.ok(file.hasOwnProperty('buffer'));
+});
